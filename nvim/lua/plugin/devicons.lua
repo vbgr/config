73f4943cdab1bc991d02019520722b60ed87e070
Vladimir Bogretsov <bogrecov@gmail.com>
2026-05-18 09:58:27 +0300

    Updated dev-icons config

--- nvim/lua/plugin/devicons.lua
+++ nvim/lua/plugin/devicons.lua
@@ -10,10 +10,23 @@ local devicons = require("nvim-web-devicons")
 
 devicons.setup({
   override = {
+    ["Dockerfile"] = { icon = "", color = "#519aba", name = "Dockerfile" },
+    ["d2"] = { icon = "󱁉", color = "#83c092", name = "d2" },
+    ["excalidraw"] = { icon = "󱁉", color = "#83c092", name = "d2" },
+    ["drawio"] = { icon = "󱁉", color = "#f2a622", name = "d2" },
+    ["excalidraw.png"] = { icon = "󱁉", color = "#83c092", name = "d2" },
+    ["py"] = { icon = "", color = "#519aba", cterm_color = "61", name = "Py" },
+    ["pyc"] = { icon = "", color = "#519aba", cterm_color = "67", name = "Pyc" },
+    ["pyd"] = { icon = "", color = "#519aba", cterm_color = "67", name = "Pyd" },
+    ["pyo"] = { icon = "", color = "#519aba", cterm_color = "67", name = "Pyo" },
     ["rst"] = { icon = "", color = "#519aba", name = "rst"},
     ["justfile"] = { icon = "", color = "#83c092", name = "Justfile" },
     ["sh"] = { icon = "", color = "#83c092", name = "shell" },
     ["zsh"] = { icon = "", color = "#83c092", name = "zsh" },
+    ["docker-compose.yml"] = { icon = "", color = "#458ee6", cterm_color = "68", name = "Dockerfile" },
+    ["docker-compose.yaml"] = { icon = "", color = "#458ee6", cterm_color = "68", name = "Dockerfile" },
+    [".dockerignore"] = { icon = "", color = "#458ee6", cterm_color = "68", name = "Dockerfile" },
+    ["dockerfile"] = { icon = "", color = "#519aba", cterm_color = "68", name = "Dockerfile" },
     ["tf"] = { icon = "", color = "#df69ba", name = "terraform" },
     ["tfvars"] = { icon = "", color = "#df69ba", name = "tfvars" },
     ["toml"] = { icon = "", color = "#83c092", name = "toml" },
